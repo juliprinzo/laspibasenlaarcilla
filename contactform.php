71bd6cfa34d1396db827ef8beb79448f4f0116ec
juliprinzo <juliprinzo@gmail.com>
2020-05-25 21:17:58 -0300

    pruebas con el contact form

--- contactform.php
+++ contactform.php
@@ -1,15 +1,15 @@
 <?php
 
-if(isset($_POST['submit'])){
-    $name= $_POST['name'];
-    $surname= $_POST['surname'];
-    $mailFrom= $_POST['mail'];
-    $message= $_POST['message'];
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
 
-    $mailTo="juliprinzo@gmail.com";
-    $headers="From: ".$mailFrom;
-    $txt="Recibiste un mail de".$name.".\n\n".$message;
+    $mailTo = "juliprinzo@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "Recibiste un mail de ".$name.".\n\n".$message;
 
     mail($mailTo, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: contacto.html?mailsend;");
 }
